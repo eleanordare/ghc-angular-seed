e71c0764b1ddb9ac598aa7547ebfa688c2f3797d
Eleanor Mehlenbacher <edare9439@gmail.com>
2017-10-02 09:20:10 -0400

    Update api.js

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET api listing. */
-router.get('/api', (req, res) => {
+router.get('/', (req, res) => {
   res.send('api works');
 });
 
